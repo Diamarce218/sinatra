183db2516d02f30c83ae59b2af8909706416afec
Diana Herran <dianam.@MacBook-Air-de-Diana.local>
2018-10-24 15:12:17 -0500

    mi version

--- solution.rb
+++ solution.rb
@@ -1,11 +1,10 @@
+
 require 'sinatra'
 
 get "/" do
   unless params[:nombre]
-    "Hola desconocido!"
     "<h1>Hola desconocido!</h1>"
   else
-    "Hola #{params[:nombre]}!"
     "<h1>Hola #{params[:nombre]}!<h1>"
   end
 end
