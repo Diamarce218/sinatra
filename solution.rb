ac4924cd387eb9adf073ab2b869b4917b579ed6d
Diana Herran <dianam.@MacBook-Air-de-Diana.local>
2018-10-24 15:03:58 -0500

    holi

--- solution.rb
+++ solution.rb
@@ -1,10 +1,9 @@
 require 'sinatra'
 
-get "/" do
+get '/' do
   if params[:nombre].present?
-    "<h1>Hola #{params[:nombre]}!<h1>"
-    
+    "<h1>Hola #{params[:nombre].capitalize}!</h1>"
   else
-    "<h1>Hola desconocido!<h1>"
+    "<h1>Hola desconocido!</h1>"
   end
 end
